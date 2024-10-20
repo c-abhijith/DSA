1962954e0622d2a1b0de55f72c9dbe0dba4b7ee5
c-abhijith <cabhijithofficial@gmail.com>
2024-10-20 14:29:14 +0530

    updated

--- DS/Tree/Binary_Tree/binarytree.py
+++ DS/Tree/Binary_Tree/binarytree.py
@@ -1,118 +1,162 @@
 class Node:
-    def __init__(self,key=None):
-        self.key = key
+    def __init__(self,data):
+        self.data = data
         self.right = None
         self.left = None
-    
-class BinaryTree:
+
+class Bt:
     def __init__(self):
-        self.root = None
-    
-    def insert(self,key):
-        new_node = Node(key)
+        self.root=None
+    def insert(self,data):
+        new_data = Node(data)
         if self.root is None:
-            self.root = new_node
-            print("Inserted")
+            self.root = new_data
+            print("insert")
             return
-        queue =[self.root]
+        queue = [self.root]
         while queue:
             current = queue.pop(0)
             if current.left is None:
-                current.left=new_node
-                print("Inserted")
+                current.left = new_data
+                print("insert")
                 return
             else:
                 queue.append(current.left)
+            
             if current.right is None:
-                current.right=new_node
-                print("Inserted")
+                current.right = new_data
+                print("Insert")
                 return
             else:
                 queue.append(current.right)
-                
-    def search_tree(self,key):
+    def search(self,key):
         if self.root is None:
-            print("There is no data in the tree")
+            return None
+        queue = [self.root]
+        while queue:
+            current=queue.pop(0)
+            if current.data == key:
+                return True
+            if current.left is not None:
+                queue.append(current.left)
+            if current.right is not None:
+                queue.append(current.right)
+        return False
+    def display(self):
+        print("Inorder-->",self.inorder(self.root))
+        print("Preorder-->",self.preorder(self.root))
+        print("postorder-->",self.postorder(self.root))
+        print("levelorder-->",self.levelorder(self.root))
+
+    def inorder(self,root):
+        result = []
+        if root:
+            result.extend(self.inorder(root.left))
+            result.append(root.data)
+            result.extend(self.inorder(root.right))
+        return result
+    
+    def preorder(self,root):
+        result = []
+        if root:
+            result.append(root.data)
+            result.extend(self.inorder(root.left))
+            result.extend(self.inorder(root.right))
+        return result
+    
+    def postorder(self,root):
+        result = []
+        if root:
+            result.extend(self.inorder(root.left))
+            result.extend(self.inorder(root.right))
+            result.append(root.data)
+        return result
+    
+    def levelorder(self,root):
+        result=[]
+        if root is None:
+            return result
+        stack =[root]
+        while stack:
+            current = stack.pop(0)
+            result.append(current.data)
+            if current.left is not None:
+                stack.append(current.left)
+            if current.right is not None:
+                stack.append(current.right)
+        return result
+    
+    def hight(self,root):
+        if root is None:
+            return -1
+        if root:
+            left = self.hight(root.left)
+            right = self.hight(root.right)
+            return max(left,right)+1
+
+    def count_node(self,root):
+        if root is None:
+            return 0
+        return 1+self.count_node(root.left)+self.count_node(root.right)
+
+    def leaf_node(self,root,leaf):
+        if root is not None:
+            if root.left is None and root.right is None:
+                leaf.append(root.data)
+            self.leaf_node(root.left,leaf)
+            self.leaf_node(root.right,leaf)
+
+    def delete_node(self,key):
+        if self.root is None:
+            print("Tree is empty")
             return
+        node = self._find_node(key)
+        if node is None:
+            print(f"The value {key} is not in the tree")
+            return
+        last_node = self.get_last_node()
+
+
+    def _find_node(self,key):
         queue = [self.root]
         while queue:
             current = queue.pop(0)
-            if current.key == key:
-                print("The value is in the tree")
-                return
-            if current.left:
-                queue.append(current.left)
-            if current.right:
-                queue.append(current.right)
-        print("The value is no in the tree")
-        
-    def inorder(self,current,result):
-        if current:
-            result = self.inorder(current.left,result)
-            result.append(current.key)
-            result=self.inorder(current.right,result)
-        return result
+            if current.data == key:
+                return current
+            if current.left: queue.append(current.left)
+            if current.right: queue.append(current.right)
+        return None
     
-    def pre_order(self,current,result):
-        if current:
-            result.append(current.key)
-            result = self.pre_order(current.left,result)
-            result = self.pre_order(current.right,result)
-        return result
-    
-    def post_order(self,current,result):
-        if current:
-            result = self.pre_order(current.left,result)
-            result = self.pre_order(current.right,result)
-            result.append(current.key)
-        return result
-        
-    def display(self):
-        inorder_result = []
-        self.inorder(self.root,inorder_result)
-        print("Inorder : ",inorder_result)
-        
-        preorder_result = []
-        self.pre_order(self.root,preorder_result)
-        print("Inorder : ",preorder_result)
-        
-        postorder_result = []
-        self.post_order(self.root,postorder_result)
-        print("Inorder : ",postorder_result)
-                
-    def delete_node(self,value):
+    def get_last_node(self):
         if self.root is None:
-            print("there is no data in the tree")
-            return
+            return None
         queue = [self.root]
-
-        key_node = None
+        last_node = None
         while queue:
-            current =queue.pop(0)
-            if current.key == value:
-                key_node=current
-            
-                        
-            
-                
-        
-tree = BinaryTree()
-tree.insert(100)
-tree.insert(200)
-tree.insert(300)
-tree.insert(400)
-tree.insert(500)
-tree.insert(600)
-tree.insert(700)
-tree.insert(800)
-tree.insert(900)
-tree.insert(1000)
-tree.insert(1100)
-tree.insert(1200)
-tree.insert(1300)
-tree.insert(1400)
-tree.insert(1500)
-tree.insert(1600)
-tree.search_tree(0)
-tree.search_tree(200)
-tree.display()
+            last_node = queue.pop(0)
+            if last_node.left: queue.append(last_node.left)
+            if last_node.right: queue.append(last_node.right)
+        return last_node
+
+
+
+
+bt = Bt()
+arr=[5,7,9,1,2,3,4]
+for i in range(len(arr)):
+    bt.insert(arr[i])
+print(bt.search(7))
+print(bt.search(90))
+bt.display()
+print("height -->",bt.hight(bt.root))
+print("count node --->",bt.count_node(bt.root))
+leaf = []
+bt.leaf_node(bt.root,leaf)
+print(leaf)
+
+bt.delete_node(3)
+bt.delete_node(78)
+
+bt.display()
+
+
